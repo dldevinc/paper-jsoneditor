d5a0bb7eedea1393132633f9f8228f9cf6eb4bdc
pix666 <x896321475@gmail.com>
2022-11-18 15:08:56 +0400

    fix critical bug

--- paper_jsoneditor/fields.py
+++ paper_jsoneditor/fields.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.fields.json import KeyTransform
 
 from . import forms
 
@@ -20,6 +21,23 @@ class JSONField(models.JSONField):
 
 
 class OrderedJSONField(JSONField):
+    def from_db_value(self, value, expression, connection):
+        if value is None:
+            return value
+
+        # Some backends (SQLite at least) extract non-string values in their
+        # SQL datatypes.
+        if isinstance(expression, KeyTransform) and not isinstance(value, str):
+            return value
+
+        if isinstance(value, str):
+            try:
+                return json.loads(value, cls=self.decoder)
+            except json.JSONDecodeError:
+                pass
+
+        return value
+
     def db_type(self, connection):
         if connection.vendor == "postgresql":
             return "json"
